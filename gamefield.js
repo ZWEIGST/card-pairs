64510dede6058a063784d57df668c371f897b0f4
ZWEIGST <watzwer@gmail.com>
2024-07-17 21:37:03 +0300

    fix the problem with choose difficulty of game

--- gamefield.js
+++ gamefield.js
@@ -1,19 +1,20 @@
 export function chooseDifficulty() {
-    let pairsCards = 0;
-    /* choose a difficuly */
+    let pairsCards;
+    
+    /* choose a difficulty */
     function setPairsCards(difficultyGame) {
-     switch (difficultyGame) {
-      case 'easy':
-        pairsCards = 3;
-        break;
-      case 'medium':
-        pairsCards = 6;
-        break;
-      case 'hard':
-        pairsCards = 9;
-        break;
-      default:
-        pairsCards = 0;
+        switch (difficultyGame) {
+            case 'easy':
+                pairsCards = 3;
+                break;
+            case 'medium':
+                pairsCards = 6;
+                break;
+            case 'hard':
+                pairsCards = 9;
+                break;
+            default:
+                pairsCards = 0;
         }
         return pairsCards;
     }
@@ -24,18 +25,19 @@ export function chooseDifficulty() {
     
     document.querySelector('.medium_level').addEventListener('click', function() {
         setPairsCards('medium');
+    });
     
-    });document.querySelector('.hard_level').addEventListener('click', function() {
+    document.querySelector('.hard_level').addEventListener('click', function() {
         setPairsCards('hard');
     });
     
     document.getElementById('start_game').addEventListener('click', function() {
-        if (pairsCards) {
+        if (pairsCards !== undefined && pairsCards !== 0) {
             console.log(`Игра запущена с выбранной сложностью: ${pairsCards} пар карточек`);
         } else {
             console.log('Выберите сложность игры');
         }
     });
+    
     return pairsCards;
-};
-
+}
